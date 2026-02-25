8272a5491d04cba83a0a72ed5bc355e9605442d1
Akeelabbas29 <akeelabbas29@gmail.com>
2026-02-25 20:50:01 +0530

    Simplify: SSM Parameter only, remove DynamoDB complexity

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,14 @@
-output "table_name" {
-  description = "Name of the DynamoDB table"
-  value       = aws_dynamodb_table.this.name
+output "parameter_name" {
+  description = "Name of the SSM parameter"
+  value       = aws_ssm_parameter.this.name
 }
 
-output "table_arn" {
-  description = "ARN of the DynamoDB table"
-  value       = aws_dynamodb_table.this.arn
+output "parameter_arn" {
+  description = "ARN of the SSM parameter"
+  value       = aws_ssm_parameter.this.arn
 }
 
-output "table_id" {
-  description = "ID of the DynamoDB table"
-  value       = aws_dynamodb_table.this.id
-}
-
-output "hash_key" {
-  description = "Hash key of the table"
-  value       = aws_dynamodb_table.this.hash_key
-}
-
-output "range_key" {
-  description = "Range key of the table (if configured)"
-  value       = aws_dynamodb_table.this.range_key
-}
-
-output "billing_mode" {
-  description = "Billing mode of the table"
-  value       = var.billing_mode
-}
-
-output "encryption_type" {
-  description = "Encryption type"
-  value       = var.kms_key_arn != "" ? "KMS (customer-managed)" : "AWS-managed"
+output "parameter_version" {
+  description = "Version of the SSM parameter"
+  value       = aws_ssm_parameter.this.version
 }
